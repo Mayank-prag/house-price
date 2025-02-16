b61230660bdc7e870341d10443e7833af56c64f8
Mayank-prag <mgarg0227@gmail.com>
2025-02-16 23:03:52 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pip install --upgrade pip
+pip install click==8.1.7 colorama==0.4.6 Flask==3.0.0 gunicorn==23.0.0 requests==2.31.0 scikit-learn==1.2.0 numpy==1.21.0 scipy==1.7.0 matplotlib==3.4.2 pandas==1.3.0
